72dd41902c10c4cb21368404f5d6244e9ae8e8a7
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2017-10-28 23:01:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(line){
+var name = ""
   if(line.length===0){
     return "The line is currently empty."
   }else {
     for(var i=0;i<line.length;i++){
-      var name = ""
       name = name + " " + (i+1) +". " +line[i]
     }
     return "The line is currently: " + name
