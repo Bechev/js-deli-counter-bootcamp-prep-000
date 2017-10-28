8703adbfca0f065cf247682681f1f2354c09e065
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2017-10-28 22:29:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var katzDeli=[];
 
-function takeANumber(katzDeliLine, name){
+/*function takeANumber(katzDeliLine, name){
 var i = 0
   while (i < katzDeliLine.length){
     i++
   }
   katzDeliLine.push(name)
   return "Welcome, "+name+" . You are number " + i+1 + " in line."
- }
+}*/
+
+ function takeANumber(katzDeliLine, name){
+  for(var i = 0; i<katzDeliLine.length; i++){
+    katzDeliLine.push(name)
+   }
+  return "Welcome, "+name+" . You are number " + i+1 + " in line."
+}
