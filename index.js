1b1e7d48a41cf5eebd7aa929b2b9b78de37abbd1
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2017-10-28 22:58:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ function currentLine(line){
   if(line.length===0){
     return "The line is currently empty."
   }else {
-
+    for(i=0;i<line.length;i++){
+      var name = (i+1) +". " +line[i]
+    }
+    return "The line is currently: " + name
   }
 }
